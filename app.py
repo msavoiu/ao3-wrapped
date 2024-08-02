064f4246622bdc4bb9e48fb57488c774ddc81363
msavoiu <savoiumadeline@gmail.com>
2024-08-01 21:19:22 -0700

    changed a few lines so function could access timeframe part of forms

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from collections import Counter
 from datetime import date
 from flask import *
 from functions import *
+import json
 import matplotlib.pyplot as plt
 import re
 import requests
@@ -35,9 +36,9 @@ def bookmarksWrapped():
         flash('Your username is invalid. Please try again!')
         return redirect('/bookmarks', code=302)
 
-    if request.form['timeframe'] == 'all time':
+    if request.form['timeframe'] == 'All time':
         fanfics = scrapeAllFanfics(username, 'bookmarks', s)
-    if request.form['timeframe'] == 'this year':
+    if request.form['timeframe'] == 'This year':
         fanfics = scrapeFanficsByYear(username, 'bookmarks', s)
 
     fandoms = []
@@ -58,16 +59,31 @@ def bookmarksWrapped():
         freeforms.extend(fanfic.freeforms)
         total_word_count += fanfic.wordcount
 
-    rating_percents = frequenciesToPercents(ratings)
+    # rating_percents = frequenciesToPercents(ratings)
     
-    generateWordcloud(freeforms)
+    # generateWordcloud(freeforms)
+
+    # # Rating frequencies to JSON for creating Chart.js pie chart
+    # rating_labels = []
+    # rating_freqs = []
+    # for rating, freq in Counter(ratings).items():
+    #     rating_labels.append(rating)
+    #     rating_freqs.append(freq)
+    # print('rating_labels', rating_labels)
+    # print('rating_freqs', rating_freqs)
+    # rating_freqs_json = json.dumps(rating_freqs)
+    # with open('static/ratingsdata.json', 'w') as file:
+    #     json.dump(rating_freqs_json, file)
+    # print("'JSON data written to ratingsdata.json'")
 
     return render_template('wrapped.html',
                            total_word_count = total_word_count,
                            total_word_count_string = f'{total_word_count:,d}',
-                           fandoms = sortedFrequencyList(fandoms)[0:5],
+                        #    fandoms = sortedFrequencyList(fandoms)[0:5],
                            ships = sortedFrequencyList(ships)[0:5],
-                           ratings = rating_percents)
+                        #    ratings = rating_percents
+                            rating_labels = Counter(ratings).keys(),
+                            rating_freqs = Counter(ratings).values())
 
 @app.route('/history')
 def history():
