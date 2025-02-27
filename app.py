b6220df7b6beac7b94638ee17f3316320ebf9079
msavoiu <savoiumadeline@gmail.com>
2025-02-26 18:14:19 -0800

    Removed waitress serve

--- app.py
+++ app.py
@@ -180,5 +180,6 @@ def historyWrapped():
                            total_word_count = format(total_word_count, ','))
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(debug=True)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=5000)
